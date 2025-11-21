50b843e223ab05685dbca6d02e9a347b4358005c
Thomas Wilkinson <thomas@capnajax.com>
2025-11-21 12:47:06 -0600

    bug fix

--- scripts/helpers/md-handler/src/file-handler.js
+++ scripts/helpers/md-handler/src/file-handler.js
@@ -375,7 +375,7 @@ async function convertMarkdownToHtml(markdownContent, title = 'Document') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${title}</title>
-  <link rel="stylesheet" href="/styles/markdown-it.css">
+  <link rel="stylesheet" type="text/css" href="/public/markdown-it.css">
 </head>
 <body>
   ${await neededIncludesPromises.header}
